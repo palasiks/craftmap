3366a5299b8874e24e5d37fcea73a3fa9c2fac49
palasiks <palasiks@upcmail.hu>
2015-10-17 02:15:38 +0200

    v0.2
    updated with 'bang removal'

--- craftmap.cpp
+++ craftmap.cpp
@@ -29,6 +29,18 @@ The inserted CraftWare comments are:
 ;segType:Skirt
 ;segType:SoftSupport
 ;segType:Support
+
+Now updated with 'bang removal', changes the feedrate of short segments to a
+preset value.
+
+The feedrate can be set by -f switch, the minimum segment length can be set by
+-l.
+
+The defaults are:
+
+  -f900 -l2
+
+Segments shorter than 2 millimeters will be written with F900 feedrate.
 */
 
 struct COMMENT_MAP {
@@ -41,7 +53,7 @@ COMMENT_MAP comment_map[] = {
  { "Loop", "Loop" },
  { "Perimeter", "Perimeter" },
  { "Pillar", "Raft" },
- { "Prime Pillar", "Raft" },
+ { "Prime Pillar", "Skirt" },
  { "Raft", "Raft" },
  { "Skirt", "Skirt" },
  { "Solid", "HShell" },
@@ -49,8 +61,6 @@ COMMENT_MAP comment_map[] = {
  { "Stacked Sparse Infill", "Infill" },
  { "Support (may Stack)", "Support" },
  { "Support Interface", "SoftSupport" },
-// { "Destring/Wipe/Jump", "" },
-// { "Jump", "" },
 };
 
 #define DIM(x) (sizeof(x)/sizeof(x[0]))
@@ -61,8 +71,108 @@ char linebuffer[0x4000];
 
 FILE *input,*output;
 
+double X,Y,F,pX,pY,written_F;
+
+double min_F = 900;
+
+double min_len = 2;
+double min_len_sq;
+
+void bang_removal()
+{
+	char *p;
+	char *fstart = 0;
+	char *fend = 0;
+	int have_EZ = 0;
+
+	X = pX;
+	Y = pY;
+
+	for (p = linebuffer; *p && *p != ';' && *p != '\n';) {
+		switch(*p++) {
+		case 'X':
+			X = strtod(p,&p);
+			break;
+
+		case 'Y':
+			Y = strtod(p,&p);
+			break;
+
+		case 'E':
+		case 'Z':
+			have_EZ = 1;
+			break;
+
+		case 'F':
+			fstart = p-1;
+			F = strtod(p,&p);
+			fend = p;
+			}
+		}
+
+	double dx = X-pX;
+	double dy = Y-pY;
+
+	pX = X;
+	pY = Y;
+
+	double lsq = dx*dx+dy*dy;
+
+	double wrF = F;
+
+	char fbuffer[32];
+
+	if (lsq < min_len_sq) {
+		if (min_F < F) {
+			if (have_EZ && lsq == 0.0) {
+				sprintf(fbuffer,"G1 F%g\n",min_F);
+
+				if (written_F != min_F)
+					fputs(fbuffer,output);
+
+				fputs(linebuffer,output);
+
+				fputs(fbuffer,output);
+
+				written_F = min_F;
+
+				return;
+				}
+			else
+				wrF = min_F;
+			}
+		}
+
+	if (fstart > linebuffer && fstart[-1] == ' ')
+		fstart--;
+
+	if (fstart && wrF == written_F)
+		memmove(fstart,fend,strlen(fend)+1);
+	else {
+		if (fstart)
+			written_F = F;
+		else
+			fstart = fend = p;
+
+		if (wrF != written_F) {
+			sprintf(fbuffer," F%g",wrF);
+
+			int l = strlen(fbuffer);
+
+			memmove(fstart+l,fend,strlen(fend)+1);
+			memmove(fstart,fbuffer,l);
+
+			written_F = wrF;
+			}
+		}
+
+	fputs(linebuffer,output);
+}
+
 void insert_color_comments(char *inname)
 {
+	min_len_sq = min_len*min_len;
+
 	input = fopen(inname,"r");
 
 	if (! input) {
@@ -75,13 +185,7 @@ void insert_color_comments(char *inname)
 
 	strcpy(outname,inname);
 
-	char *ext = strrchr(outname,'.');
-	char *tmp = ".$$$";
-
-	if (ext)
-		strcpy(ext,tmp);
-	else
-		strcat(outname,tmp);
+	strcat(outname,".$$$");
 
 	output = fopen(outname,"w");
 
@@ -90,7 +194,9 @@ void insert_color_comments(char *inname)
 		return;
 		}
 
-	while (fgets(linebuffer,sizeof(linebuffer),input)) {
+	int translated = 0;
+
+	while (fgets(linebuffer,sizeof(linebuffer)-1,input)) {
 		const char *segtype = 0;
 		const char *tail = 0;
 
@@ -114,10 +220,22 @@ void insert_color_comments(char *inname)
 				}
 			}
 
-		fputs(linebuffer,output);
+ // allow reprocess
+		if (translated && linebuffer[0] == ';' && memcmp(linebuffer,";segType:",9) == 0)
+			continue;
 
-		if (segtype)
+ // G0 and G1 lines handled separately
+		if (linebuffer[0] == 'G' && 
+		    (linebuffer[1] == '0' || linebuffer[1] == '1') &&
+		    (linebuffer[2] < '0' || linebuffer[2] > '9'))
+			bang_removal();
+		else
+			fputs(linebuffer,output);
+
+		if (segtype) {
 			fprintf(output,";segType:%s\n",segtype);
+			translated++;
+			}
 		}
 
 	fclose(input);
@@ -138,13 +256,25 @@ void insert_color_comments(char *inname)
 int main(int argc,char **argv)
 {
 	if (argc < 2) {
-		printf(	"Usage: craftmap gcode_file ...\n");
+		printf(	"Usage: craftmap gcode_file ...\n"
+			"\n"
+			"feedrate for short segments: -f#\n"
+			"length of short segments: -l#\n");
 
 		return 1;
 		}
 
-	for (int i=1; i<argc; i++)
-		insert_color_comments(argv[i]);
+	for (int i=1; i<argc; i++) {
+		if (argv[i][0] == '-') {
+			if (argv[i][1] == 'f')
+				min_F = atof(argv[i]+2);
+
+			if (argv[i][1] == 'l')
+				double min_len = atof(argv[i]+2);
+			}
+		else
+			insert_color_comments(argv[i]);
+		}
 
 	return 0;
 }
